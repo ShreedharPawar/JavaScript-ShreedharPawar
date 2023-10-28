99c56129e0c349a4766b98f724fc4ea23f23030e
ShreedharPawar <129543175+ShreedharPawar@users.noreply.github.com>
2023-10-28 17:19:31 +0000

    Data Conversition in one another

---
+++ 01_basics/06_DataConversion.js
@@ -0,0 +1,23 @@
+let a = null
+let score="33abc"
+console.log(typeof(score))
+
+let valueNumber=Number(score)// Changing the given string in number
+console.log(typeof valueNumber)
+console.log(valueNumber)//The output will be NaN= Not a number
+
+//String==Number
+//"33"==33
+//"33abc"==NaN
+let isLoggedIn =1
+let BooleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(BooleanIsLoogedIn)
+console.log(typeof BooleanIsLoogedIn)
+//1==True
+//0==Fale viceVersa
+//""=false
+//"Shreedhar"=True
+let num=43
+let StringNum=String(num)
+console.log(StringNum)
+console.log(typeof StringNum)
